b7c23c1927d55833df4b0bf05c9d07ad92c094c2
williamxiao <xiaoyanzhe2002@gmail.com>
2023-11-11 21:50:21 -0500

    create new page /submit to manage the data from add.html, implement the code for adding items to the db

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 # Local imports
 from base import app, db
 from models import User, FridgeItem
+from datetime import datetime
+
 
 # 3rd party imports
-from flask import request, render_template
+from flask import request, render_template, flash, redirect
 
 
 # Create the database
 with app.app_context():
     db.create_all()
+    db.session.add(User(name='Main'))
+    db.session.commit()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -18,6 +22,21 @@ def index():
 @app.route('/add-item')
 def add_item():
     return render_template('add.html')
+
+@app .route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    expiry_date = request.form.get('expiry_date')
+
+     # Convert expiry_date from string to datetime object
+    expiry_date = datetime.strptime(expiry_date, '%Y-%m-%d')
+    
+    # Create a new FridgeItem instance
+    new_item = FridgeItem(name=name, expiry_date=expiry_date, user_id=1)
+    db.session.add(new_item)
+    db.session.commit()
+    flash('New ingredient added successfully!')
+    return redirect('/add-item')
 
 if __name__ == '__main__':
     app.run(debug=True)
