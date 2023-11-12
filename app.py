1323bfb1dd26d5a7310cde4df84e3e9d3886a73a
williamxiao <xiaoyanzhe2002@gmail.com>
2023-11-12 11:22:01 -0500

    Finish delete

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # Local imports
 from base import app, db
 from models import User, FridgeItem, get_all_fridge_items
+from datetime import datetime
 from recipe import get_recipes, get_image
 
 # 3rd party imports
-from flask import request, render_template, redirect, flash
-
-from datetime import datetime
+from flask import url_for, request, render_template, redirect, flash
 
 # Create the database
 with app.app_context():
@@ -26,12 +25,13 @@ def fridge():
         image_url = get_image(recipe)
         return render_template('recipe.html', recipe=recipe, image_url=image_url)
 
+
 @app.route('/')
 def starter():
     return render_template('index.html')
 
 
-@app.route('/add-item')
+@app.route('/add_item')
 def add_item():
     return render_template('add.html')
 
@@ -49,7 +49,19 @@ def submit():
     db.session.add(new_item)
     db.session.commit()
     flash('New ingredient added successfully!')
-    return redirect('/add-item')
+    return redirect('/add_item')
+
+@app.route('/delete_item/<int:item_id>', methods=['POST'])
+def delete_item(item_id):
+    item_to_delete = FridgeItem.query.get(item_id)
+    if item_to_delete:
+        db.session.delete(item_to_delete)
+        db.session.commit()
+        flash("Ingredient deleted successfully!")
+    else :
+        flash("Looks like you tried to delete nothing...")
+    return redirect(url_for('fridge'))
+
 
 
 if __name__ == '__main__':
