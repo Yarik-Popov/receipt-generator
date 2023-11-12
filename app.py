2fb2892865c741c353191c18041dabf4c23f7ce3
Mini-danger <73896686+Will-xyz2002@users.noreply.github.com>
2023-11-12 15:33:00 -0500

    set cooking.html done (#27)
    Co-authored-by: Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>

--- app.py
+++ app.py
@@ -55,16 +55,11 @@ def submit():
 
 @app.route('/cooking', methods=['GET','POST'])
 def cooking():
-    if request.method == 'GET':
-        items = get_all_fridge_items()
-        return render_template('cooking.html', items=items)
-    else:
-        requested_items = request.form.getlist("items")
-        recipe = get_recipes([], requested_items)  # No seasonings
-        image_url = get_image(recipe)
-        return render_template('recipe.html', recipe=recipe, image_url=image_url)
-
-
+    return render_template('cooking.html')
+   
+@app.route('/get_items', methods=['GET', 'POST'])
+def get_items():
+    return "cooking works well!"
 
 @app.route('/delete_item/<int:item_id>', methods=['POST'])
 def delete_item(item_id):
